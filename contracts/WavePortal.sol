4e21d5c90d48b83fe61f8e6280483afd7948aa87
rohit-710 <rrohitramesh710@gmail.com>
2022-03-31 14:14:21 +0530

    Final Commit

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -6,18 +6,122 @@ import "hardhat/console.sol";
 
 contract WavePortal{
 
+    constructor() payable {
+        console.log("We have been constructed!");
+
+        seed = (block.timestamp + block.difficulty) % 100; //Setting the initial seed
+    }
     uint256 totalWaves;
+    uint256 private seed; //To generate a random number
+    
+    //Creating a new event
+    //An event is an inheritable member of the contract, which stores the arguments passed in the 
+    //transaction logs when emitted. Generally, events are used to inform the calling application about the current state of the contract, with the help of the logging facility of EVM.
+    event showWave(address indexed from, uint256 timestamp, string message);
 
-    constructor() 
-    {
-        console.log("Hello, I am a Wave App that has come to life through Solidity and you've got a friend in me :)");
+    
+    //I created a struct here named Wave which stores some of the information of every wave waved.
+    //It stores address of the waver, the message put in by the waver and the time stamp of when the wave was waved. 
+    //A struct is basically a custom datatype where we can customize what we want to hold inside it.
+    
+    struct Wave {
+        address waver; // The address of the user who waved.
+        string message; // The message the user sent.
+        uint256 timestamp; // The timestamp when the user waved.
     }
 
-    function newWave() public
+     
+    
+
+    //We declare a variable waves of the datatype "Wave" that lets us store an array of structs.
+    //This is what lets us hold all the waves anyone ever sends to us!
+    Wave[] waves;
+    /*constructor() 
     {
+        console.log("I am the smart contract and I have just come to life. Let's go!");
+    }*/
+
+    /*
+     * This is an address => uint mapping, meaning I can associate an address with a number!
+     * In this case, I'll be storing the address with the last time the user waved at us.
+     */
+    mapping(address => uint256) public lastWavedAt;
+
+    function newWave(string memory _message) public
+    {
+        /*
+         * We need to make sure the current timestamp is at least 30 seconds more than the last timestamp we stored
+         */
+        require(lastWavedAt[msg.sender] + 30 seconds < block.timestamp, "Must wait 30 seconds before waving again.");
+
+        /*
+         * Update the current timestamp we have for the user
+         */
+        lastWavedAt[msg.sender] = block.timestamp;
+
         totalWaves+=1;
-        console.log("%s has just added me as a friend and waved at me. ", msg.sender);
+        console.log("%s has just added me as a friend and waved at me. ", msg.sender, _message);
+
+        //This is where we store the message and the details of the wave to the array.
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+
+        //Generate a new seed 
+        seed = (block.difficulty + block.timestamp + seed) % 100;
+
+        console.log("Random # generated: %d", seed);
+
+        //Now we give the user a 50% chance to win
+         if (seed <= 50) {
+            console.log("%s won!", msg.sender);
+
+            /*
+             * The same code we had before to send the prize.
+             */
+            uint256 prizeAmount = 0.0001 ether;
+            require(
+                prizeAmount <= address(this).balance,
+                "Trying to withdraw more money than the contract has."
+            );
+            (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+            require(success, "Failed to withdraw money from contract.");
+        }
+
+        //Now, we emit the event details
+        //In order to make events stand out with regards to regular function calls, emit EventName() 
+        //as opposed to just EventName() should now be used to "call" events.
+
+        emit showWave(msg.sender, block.timestamp, _message);
+
+        /*
+
+        //To start we're just going to give everyone who waves at us 0.0001 ETH. Which is $0.31. 
+        //And this is all happening on testnet, so, it's fake $!
+
+        uint256 prizeAmount = 0.0001 ether;
+
+        //require which basically checks to see that some condition is true.
+        //If it's not true, it will quit the function and cancel the transaction.
+
+        //address(this).balance is the balance of the contract itself.
+        require(
+            prizeAmount <= address(this).balance,
+            "Trying to withdraw more money than the contract has."
+            );
+        (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+        require(success, "Failed to withdraw money from contract.");
+
+        */
+
     }
+
+    //We create a function getAllWaves which will return the struct array, waves, to us.
+    //This will make it easy to retrieve the waves from our website!
+    
+    function getAllWaves() public view returns (Wave[] memory) {
+        return waves;
+    }
+
+
     // msg.sender, this is the wallet address of the person who called the function. It's like built-in authentication. 
     //We know exactly who called the function because in order to even call a smart contract function, 
     //you need to be connected with a valid wallet!
--- scripts/run.js
+++ scripts/run.js
@@ -3,7 +3,7 @@ const main = async () => {
     //n order to deploy something to the blockchain, we need to have a wallet address! Hardhat does this 
     //for us magically in the background, but here I grabbed the wallet address of contract owner and I also 
     //grabbed a random wallet address and called it randomPerson.
-    const [owner, randomPerson] = await hre.ethers.getSigners();
+    //Syntax: const [owner, randomPerson] = await hre.ethers.getSigners();
 
 
     //This will actually compile our contract and generate the necessary files we need to work 
@@ -14,7 +14,11 @@ const main = async () => {
     //Then, after the script completes it'll destroy that local network. So, every time you run the contract, it'll 
     //be a fresh blockchain. What's the point? It's kinda like refreshing your local server every time so you 
     //always start from a clean slate which makes it easy to debug errors.
-    const waveContract = await waveContractFactory.deploy();
+    const waveContract = await waveContractFactory.deploy(
+      {
+        value: hre.ethers.utils.parseEther("0.1"),
+      }
+    );
 
     //We'll wait until our contract is officially deployed to our local blockchain! Our constructor runs when we actually deploy.
     await waveContract.deployed();
@@ -24,7 +28,16 @@ const main = async () => {
     //blockchain. So, this address gives us easy access to the contract we're interested in working with! This will be more 
     //important a bit later once we deploy to a real Ethereum network.
     console.log("Contract deployed to:", waveContract.address);
-    console.log("Contract deployed by:", owner.address);
+    //console.log("Contract deployed by:", owner.address);
+
+    //Get contract Balance
+    let contractBalance = await hre.ethers.provider.getBalance(
+      waveContract.address
+    );
+    console.log(
+      "Contract balance:",
+      hre.ethers.utils.formatEther(contractBalance)
+    );
 
     //First I call the function to grab the # of total waves. Then, I do the wave. Finally, I grab the waveCount 
     //one more time to see if it changed.
@@ -32,15 +45,29 @@ const main = async () => {
     let waveCount;
     waveCount = await waveContract.showTotalWaves();
     
-    let waveTxn = await waveContract.newWave();
-    await waveTxn.wait();
+    const waveTxn = await waveContract.newWave("This is wave #1");
+    await waveTxn.wait(); //waits for contract to be mined
     
-    waveCount = await waveContract.showTotalWaves();
+    const waveTxn2 = await waveContract.newWave("This is wave #2");
+    await waveTxn2.wait()
+    const [_, randomPerson] = await hre.ethers.getSigners();
 
+    //Get Contract Balance
+    contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+    console.log(
+      "Contract balance:",hre.ethers.utils.formatEther(contractBalance)
+      );
+
+    /*
+    
     //The below code is to allow someone to send us a wave, and make our website interactive for users. 
-    waveTxn = await waveContract.connect(randomPerson).newWave();
-    await waveTxn.wait();
-    waveCount = await waveContract.showTotalWaves();
+    waveTxn = await waveContract.connect(randomPerson).newWave("Another message!");
+    await waveTxn.wait(); // Wait for the transaction to be mined
+
+    */
+
+    let allWaves = await waveContract.getAllWaves();
+    console.log(allWaves);
   };
   
   const runMain = async () => {
